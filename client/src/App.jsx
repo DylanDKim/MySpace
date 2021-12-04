c2725a10026970adc74bf5c5cf3670b8be4e2319
Dylan <dohyunk21@gmail.com>
2021-12-04 12:06:20 -0800

    print pathname

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,13 +6,19 @@ import Twitter from './components/Twitter/Twitter.jsx';
 import Login from './components/Login.jsx';
 import { TitleHeader, RightFlex, DownFlex } from './components/styles/main.js'
 
-// https://www.instagram.com/oauth/authorize?client_id=263221255869371&redirect_uri=https://localhost:443/&scope=user_profile,user_media&response_type=code
+// https://api.instagram.com/oauth/authorize?app_id=263221255869371&redirect_uri=18.119.158.29&scope=user_profile,user_media&response_type=code
+
+//?u=https%3A%2F%2F18.119.158.29%2F%3Fcode%3DAQDMkuq8olFjByHliPCBkm0p8gcVbcgKOtqYJAskXuA6Majcx8PX5AUFpN05HUOWLLtCjZsG3eN9A6tUZH4ESW46yANgmAkt11NxCbqgi_wBeVzZmHKgOwrNrXHWJklBSwPX6mZAK7bYIoAby5LItRToWfSTnIP_xRI_4F0VfiEz45QBog1ho1phUY39H72bzVkQNvm3datarIlWzANCljoOpSS-d-PX-Ys_OOwinsoLng%23_&e=ATMVVuxqLLfvkecjQ8rr60THqs55j-7I-ebqmBypE7bvcJIf9YQuqDeGQw8b4oCI6crYxWXHnrO0rBCi1yczO4uyusKhp_RvyMQScA&s=1
 
 function App(props) {
   const [name, setName] = useState('Dylan Kim');
   const [nameSet, setNameSet] = useState(false);
   useEffect(() => {
     axios.post('https://api.instagram.com/oauth/access_token')
+  })
+
+  useEffect(() => {
+    console.log(widow.location.pathname);
   })
 
   return (
